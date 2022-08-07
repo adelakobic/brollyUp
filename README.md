1db9ab500c4b0ac8da92230627c5d7944b022d60
adelakobic <alojzamauder@yahoo.co.uk>
2022-08-07 11:26:03 +0100

    updates to CSS adding flex wrap and adjusting to buttons  and JS to fetch London in results

--- README.md
+++ README.md
@@ -44,7 +44,8 @@ This image shows the weather application appearance and functionality.
 * created the basic grid layout using Bootstrap - for the 5-day weather forecast section and search for a city
 * adjusted the background colour for 5-day weather
 * added cards for the Home weather section and the 5-day weather forecast
-* GIT REVERT : back to commit dd980235652e11bc5a93f038a887596a1a62d3b3 - revert failed in Terminal having to delet
+* GIT REVERT : back to commit dd980235652e11bc5a93f038a887596a1a62d3b3 - revert failed in Terminal having to delete code and try to revert back to where I was at 13:19 on 6/8/22
+* created new JS doc (script2) to re code the API key and 
 
 
 ## Link to the deployed website 💻
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -4,7 +4,7 @@ const APIkey = '8ae4d5b245e37bca05b9a458cedd88f1';
 const btn = document.querySelector(".btn");  //DO NOT MESS
 
 function getApi() {}
-    // fetch request to get API link - this works - don't touch
+    // fetch request to get API link - this works - don't touch v this block - API KEY
     var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=8ae4d5b245e37bca05b9a458cedd88f1';
     btn.onclick = () => {
   
@@ -15,5 +15,37 @@ function getApi() {}
       .then(function (data) {
         console.log(data)
       });
-  
 } // dont touch ^ this block
+
+//endpoint
+var endPoint = "https://api.openweathermap.org/data/2.5/onecall?";
+
+// city Search section - if not working API key - remove the var SearchFormEl
+var searchFormEl = document.querySelector('#city-search')
+function citySearchSubmit(event) {
+    event.preventDefault();
+
+    var searchInputVal = document.querySelector('#searchCity').value;
+    if (!searchInputVal) {
+        console.error('Enter city please.');
+        return;
+    }
+}
+var cityName = 'London';
+// from: https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys 
+var searchForCity = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&appid=" + APIkey;
+console.log(searchForCity);
+
+function getCity() {}
+    // fetch request to get city & API link
+    var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&appid=" + APIkey;
+    btn.onclick = () => {
+  
+    fetch(queryUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+      });
+} // not working yet
